39d51b17d04cb27df6a96edb25b5aa3671205eac
Rahul <115698783+architectrahul@users.noreply.github.com>
2024-06-13 19:05:37 +0530

    Update

---
+++ sandbox/s3.tf
@@ -0,0 +1,4 @@
+resource "aws_s3_bucket" "sandbox_bucket" {
+  bucket = "aft-new-${data.aws_caller_identity.current.account_id}"
+  acl    = "private"
+}
--- sandbox/terraform/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "learn-tf-bucket-rahul"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
