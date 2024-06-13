85cc3baf814f4bc78a8f0ee74f74f3fdfc2f48a1
Rahul <115698783+architectrahul@users.noreply.github.com>
2024-06-13 19:08:11 +0530

    Update

--- sandbox/s3.tf
+++ sandbox/s3.tf
@@ -1,4 +1,4 @@
 resource "aws_s3_bucket" "sandbox_bucket" {
-  bucket = "aft-new-${data.aws_caller_identity.current.account_id}"
+  bucket = "aft-new-${data.aws_caller_identity.current.account_id}-new"
   acl    = "private"
 }
